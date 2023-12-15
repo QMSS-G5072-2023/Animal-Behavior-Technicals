b006f6035bdc543f732ac045d18726d9a3678564
arushisaranath <arushisaranath@gmail.com>
2023-12-15 14:50:57 -0500

    files modified

--- animal_behavior_technicals/main.py
+++ animal_behavior_technicals/main.py
@@ -1,14 +1,37 @@
-# main.py
 import utils
 
 def main():
-    status = 'A'
-    result = utils.is_status_accurate(status)
+    # Check if eobs_status is accurate
+    eobs_status = 'A'
+    result_status = utils.is_status_accurate(eobs_status)
 
-    if result:
-        print(f"The eobs_status is '{status}'.")
+    if result_status:
+        print(f"The eobs_status is accurate.")
     else:
-        print(f"The eobs_status is not '{status}'.")
+        print(f"The eobs_status is not accurate.")
+
+    # Check if temperature is high
+    eobs_temperature_high = 36
+    result_high_temperature = utils.is_high_temperature(eobs_temperature_high)
+
+    if result_high_temperature:
+        print(f"The temperature ({eobs_temperature_high}°C) is high.")
+    else:
+        print(f"The temperature ({eobs_temperature_high}°C) is not high.")
+
+    # Check if temperature is low
+    eobs_temperature_low = 1
+    result_low_temperature = utils.is_low_temperature(eobs_temperature_low)
+
+    if result_low_temperature:
+        print(f"The temperature ({eobs_temperature_low}°C) is low.")
+    else:
+        print(f"The temperature ({eobs_temperature_low}°C) is not low.")
+
+    # Get climate zone for a specific location
+    location_latitude = 25
+    climate_zone = utils.get_climate_zone(location_latitude)
+    print(f"The climate zone for latitude {location_latitude} is: {climate_zone}")
 
 if __name__ == "__main__":
     main()
--- animal_behavior_technicals/utils.py
+++ animal_behavior_technicals/utils.py
@@ -1,3 +1,42 @@
 def is_status_accurate(eobs_status):
     """Check if eobs_status is accurate."""
     return eobs_status == 'A'
+
+def is_high_temperature(eobs_temperature):
+    return eobs_temperature > 35
+
+# Example usage:
+temperature = 36
+result = is_high_temperature(temperature)
+
+if result:
+    print(f"The temperature ({temperature}°C) is high.")
+else:
+    print(f"The temperature ({temperature}°C) is not high.")
+
+def is_low_temperature(eobs_temperature):
+    return eobs_temperature < 3
+
+# Example usage:
+temperature = 1
+result = is_low_temperature(temperature)
+
+if result:
+    print(f"The temperature ({temperature}°C) is low.")
+else:
+    print(f"The temperature ({temperature}°C) is not low.")
+
+def get_climate_zone(location_lat):
+    if -30 <= location_lat <= 30:
+        return 'Tropical'
+    elif -60 <= location_lat < -30 or 30 < location_lat <= 60:
+        return 'Temperate'
+    elif -90 <= location_lat < -60 or 60 < location_lat <= 90:
+        return 'Polar'
+    else:
+        return 'Unknown'
+
+# Example usage:
+latitude = 25 
+climate_zone = get_climate_zone(latitude)
+print(f"The climate zone for latitude {latitude} is: {climate_zone}")
